a91027a6b8e17b96386ae9c0977c60f2650139db
Reksa Syahputra <reksa.prayoga1012@gmail.com>
2023-09-06 21:04:29 +0700

    main

---
+++ assets/js/scripts.js
@@ -0,0 +1,30 @@
+import Splide from "https://cdn.skypack.dev/@splidejs/splide@2.4.21"
+import Grid from "https://cdn.skypack.dev/@splidejs/splide-extension-grid@0.2.0"
+
+new Splide('#example-grid', {
+    type: 'loop', // スライダーの種類 @type: { string }, default: 'slide'
+    height: '20rem', // スライダーの高さ @type: { number }, default: ０
+    gap: '1em', // スライド間のギャップサイズ @type: { number|string }, default: ０
+    perPage: 2, // 1ページに表示するスライドの数 @type: { number }, default: ０
+    perMove: 1, // スライダーが移動するスライドの数 @type: { number }, default: ０
+    grid: {
+        dimensions: [[1, 1], [2, 2], [1, 1], [2, 2], [1, 1], [2, 2]], // 配列としてのディメンション（行と列）のコレクション @type: { Array|boolean }, default: false
+        gap: { // オブジェクトとしての行と列のギャップサイズ @type: { Object }, default: {}
+            row: '1em', // 行の数 @type: { number }, default: 1
+            col: '1em' // 列の数 @type: { number }, default: 1
+        }
+    },
+    breakpoints: {  // ブレークポイントの定義
+        600: {
+            height: '10rem',
+            perPage: 1,
+            grid: {
+                dimensions: [[2, 2], [1, 1], [2, 1], [1, 2], [2, 2], [1, 3], [3, 1]],
+                gap: {
+                    row: '.5em',
+                    col: '.5em'
+                }
+            }
+        }
+    }
+}).mount({ Grid })
