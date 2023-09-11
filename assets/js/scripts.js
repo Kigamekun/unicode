72ff628c96cc6b8b8c454cdb07e1b1e154b348c0
Reksa Syahputra <reksa.prayoga1012@gmail.com>
2023-09-12 05:52:40 +0700

    push an partner

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,17 +5,17 @@ new Splide('#example-grid', {
     type: 'loop', // スライダーの種類 @type: { string }, default: 'slide'
     height: '20rem', // スライダーの高さ @type: { number }, default: ０
     gap: '1em', // スライド間のギャップサイズ @type: { number|string }, default: ０
-    perPage: 2, // 1ページに表示するスライドの数 @type: { number }, default: ０
+    perPage: 4, // 1ページに表示するスライドの数 @type: { number }, default: ０
     perMove: 1, // スライダーが移動するスライドの数 @type: { number }, default: ０
     grid: {
-        dimensions: [[1, 1], [2, 2], [1, 1], [2, 2], [1, 1], [2, 2]], // 配列としてのディメンション（行と列）のコレクション @type: { Array|boolean }, default: false
+        dimensions: [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]], // 配列としてのディメンション（行と列）のコレクション @type: { Array|boolean }, default: false
         gap: { // オブジェクトとしての行と列のギャップサイズ @type: { Object }, default: {}
             row: '1em', // 行の数 @type: { number }, default: 1
             col: '1em' // 列の数 @type: { number }, default: 1
         }
     },
     breakpoints: {  // ブレークポイントの定義
-        600: {
+        1000: {
             height: '30rem',
             perPage: 2,
             grid: {
