9ee83fddcf8fbd69c6fc9fba9ae8e293549a7d4c
Reksa Syahputra <reksa.prayoga1012@gmail.com>
2023-09-13 21:54:38 +0700

    push an new

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,3 +26,31 @@ new Splide('#example-grid', {
         }
     }
 }).mount({ Grid })
+
+
+
+// new Splide('#splidePortfolio', {
+//     type: 'loop', // スライダーの種類 @type: { string }, default: 'slide'
+//     height: '50rem', // スライダーの高さ @type: { number }, default: ０
+//     gap: '1em', // スライド間のギャップサイズ @type: { number|string }, default: ０
+//     perPage: 3, // 1ページに表示するスライドの数 @type: { number }, default: ０
+//     perMove: 1, // スライダーが移動するスライドの数 @type: { number }, default: ０
+//     grid: {
+//         dimensions: [[2, 2], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]], // 配列としてのディメンション（行と列）のコレクション @type: { Array|boolean }, default: false
+//         gap: { // オブジェクトとしての行と列のギャップサイズ @type: { Object }, default: {}
+//             row: '1em', // 行の数 @type: { number }, default: 1
+//             col: '1em' // 列の数 @type: { number }, default: 1
+//         }
+//     },
+//     breakpoints: {  // ブレークポイントの定義
+//         1000: {
+//             height: '10rem',
+//             perPage: 2,
+//             grid: {
+//                 dimensions: [[1, 1], [1, 1], [1, 1], [1, 1]],
+//                 gap: {
+//                 }
+//             }
+//         }
+//     }
+// }).mount({ Grid })
